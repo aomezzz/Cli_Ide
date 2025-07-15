f4bbfaea65c8bc169722236f222813337675c7b8
aomezzz <aomezzz.aomsin@gmail.com>
2025-07-15 10:20:53 +0700

    อัปเดตไฟล์ล่าสุด

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("Hello Node.js");
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  console.log("มีการเรียกหน้า /");
+  res.send("Hello Debugger!");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
+
+console.log("สวัสดีจาก CLI IDE Project!");
